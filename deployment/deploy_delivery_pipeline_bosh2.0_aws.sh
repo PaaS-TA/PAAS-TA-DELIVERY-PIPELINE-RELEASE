a4d4405eb39ab4cc231a3a090f7fca735d41c12b
swmoon <swmoon@bluedigm.com>
2019-09-17 13:47:46 +0900

    ubuntu-xenial 지원되도록 변경 ubuntu-trusty 미지원

--- deployment/deploy_delivery_pipeline_bosh2.0_aws.sh
+++ deployment/deploy_delivery_pipeline_bosh2.0_aws.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # stemcell 버전은 3445.2 버전으로 사용하시고 https://github.com/PaaS-TA/Guide-2.0-Linguine-/blob/master/Download_Page.md 에서 다운받아 쓰십시요.
 
-bosh -d paasta-delivery-pipeline-service deploy paasta_delivery_pipeline_bosh2.0_aws.yml \
+bosh -d paasta-delivery-pipeline-service deploy paasta_delivery_pipeline_bosh2.0.yml \
    -o use-public-network-aws.yml \
    -v internal_network_name=default \
    -v public_network_name=vip \
